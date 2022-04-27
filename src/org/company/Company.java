82ca753b85d6f6f4e4a3b27287153a79cdef4063
haroonrazeeth <hrazeeth@gmail.com>
2022-04-27 21:38:42 +0530

    2nd edit

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -5,11 +5,17 @@ public class Company {
 	public void companyName() {
 		System.out.println("company name is ABC");
 	}
+
+	public void companyBranches() {
+		System.out.println("company have 5 branches");
+	}
+
 	public void companyId() {
 		System.out.println("company Id is 123");
 	}
+
 	public void companyLoc() {
 		System.out.println("company name is OMR");
 	}
-	
+
 }
