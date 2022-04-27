0cd778dc258f71f132f0a48240598266301c6e52
haroonrazeeth <hrazeeth@gmail.com>
2022-04-27 20:59:50 +0530

    270422

---
+++ src/org/company/Company.java
@@ -0,0 +1,15 @@
+package org.company;
+
+public class Company {
+
+	public void companyName() {
+		System.out.println("company name is ABC");
+	}
+	public void companyId() {
+		System.out.println("company Id is 123");
+	}
+	public void companyLoc() {
+		System.out.println("company name is OMR");
+	}
+	
+}
